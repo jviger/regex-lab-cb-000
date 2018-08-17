cdfd6ef5668d99ca3c7b8b316b6cb4649f7ea183
Jason Viger <jason.viger@gmail.com>
2018-08-17 08:03:08 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,7 +16,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
- if text.scan(/^[A-F]\w[!?\.,]/)
+ text.scan(/^[A-F]\w[!?\.,]/)
 end
 
 def valid_phone_number?(phone)
