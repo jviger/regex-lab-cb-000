39a6f91f030cd5211c1c179c71562f12321d2950
Jason Viger <jason.viger@gmail.com>
2018-08-17 07:16:29 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -6,7 +6,7 @@ end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  if word.match(/\b(un)/) && word.match(/\w+ing/)
+  if text.match(/\b(un)/) && text.match(/\w+ing/)
   return TRUE
 else return FALSE
 end
