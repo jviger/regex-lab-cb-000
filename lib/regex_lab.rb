40249876e40f0531a39a52ad040cc34b7c073015
Jason Viger <jason.viger@gmail.com>
2018-08-17 08:05:05 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,7 +16,9 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
- text.scan(/^[A-F]/)
+ if text.scan(/\A[A-Z].+[.,?!]\z/)
+   return TRUE
+ else false
 end
 
 def valid_phone_number?(phone)
